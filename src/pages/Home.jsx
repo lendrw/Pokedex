3a62a2013764d508d4f6d6d66fcb5e1cd95cec64
Leandro Freire <88214669+lendrw@users.noreply.github.com>
2025-03-08 16:52:23 -0300

    .

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -31,23 +31,23 @@ const Home = () => {
                     <Link to="/Pokedex/kanto" className={styles.link}>
                         <li className={`press ${styles.kanto}`}>Kanto Pokédex</li>
                     </Link>
-                    <Link to="/Pokedex/johto" className={styles.link}>
-                        <li className={`press ${styles.johto}`}>Johto Pokédex</li>
-                    </Link>
-                    <Link to="/Pokedex/hoenn" className={styles.link}>
-                        <li className={`press ${styles.hoenn}`}>Hoenn Pokédex</li>
-                    </Link>
-                    <Link to="/Pokedex/sinnoh" className={styles.link}>
-                        <li className={`press ${styles.sinnoh}`}>Sinnoh Pokédex</li>
-                    </Link>
                     <Link to="/Pokedex/unova" className={styles.link}>
                         <li className={`press ${styles.unova}`}>Unova Pokédex</li>
+                    </Link>
+                    <Link to="/Pokedex/johto" className={styles.link}>
+                        <li className={`press ${styles.johto}`}>Johto Pokédex</li>
                     </Link>
                     <Link to="/Pokedex/kalos" className={styles.link}>
                         <li className={`press ${styles.kalos}`}>Kalos Pokédex</li>
                     </Link>
+                    <Link to="/Pokedex/hoenn" className={styles.link}>
+                        <li className={`press ${styles.hoenn}`}>Hoenn Pokédex</li>
+                    </Link>
                     <Link to="/Pokedex/alola" className={styles.link}>
                         <li className={`press ${styles.alola}`}>Alola Pokédex</li>
+                    </Link>
+                    <Link to="/Pokedex/sinnoh" className={styles.link}>
+                        <li className={`press ${styles.sinnoh}`}>Sinnoh Pokédex</li>
                     </Link>
                     <Link to="/Pokedex/galar" className={styles.link}>
                         <li className={`press ${styles.galar}`}>Galar Pokédex</li>
